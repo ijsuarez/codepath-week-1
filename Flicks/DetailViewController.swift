acb413366a7b6f2dc64682305b7044091a2e8354
Labuser <Labuser@wtk316-04.cec.wustl.edu>
2016-01-25 18:30:18 -0600

    finished week 2's preliminary monday work

---
+++ Flicks/DetailViewController.swift
@@ -0,0 +1,70 @@
+//
+//  DetailViewController.swift
+//  Flicks
+//
+//  Created by Labuser on 1/25/16.
+//  Copyright © 2016 Isaias Suarez. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    var movie: NSDictionary!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let title = movie["title"] as! String
+        titleLabel.text = title
+        let overview = movie["overview"]
+        overviewLabel.text = overview as? String
+        
+        if let posterPath = movie["poster_path"] as? String {
+            let baseUrl = "http://image.tmdb.org/t/p/w500"
+            let imageRequest = NSURLRequest(URL: NSURL(string: baseUrl + posterPath)!)
+            posterImageView.setImageWithURLRequest(imageRequest, placeholderImage: nil, success: {(imageRequest, imageResponse, image) -> Void in
+                if imageResponse != nil {
+                    print("Image not cached, fade in image")
+                    self.posterImageView.alpha = 0.0
+                    self.posterImageView.image = image
+                    UIView.animateWithDuration(0.3, animations: {() -> Void in
+                        self.posterImageView.alpha = 1.0
+                    })
+                } else {
+                    print("Image was cached")
+                    self.posterImageView.image = image
+                }
+            },
+            failure: {(imageRequest, imageResponse, error) -> Void in
+                self.posterImageView.image = nil
+            })
+        }
+        else {
+            posterImageView.image = nil
+        }
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -236,14 +236,20 @@ class MoviesViewController: UIViewController, UITableViewDataSource,UITableViewD
         view.endEditing(true) //this doesn't seem to work?
     }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        let cell = sender as! UICollectionViewCell
+        let indexPath = collectionView.indexPathForCell(cell)
+        let movie = movies![indexPath!.row]
+        
+        let detailViewController = segue.destinationViewController as! DetailViewController
+        detailViewController.movie = movie
     }
-    */
+    
 
 }
